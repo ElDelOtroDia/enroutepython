a6a078e5441b51e0500b218173679d50ac170826
ElDelOtroDia <luis_carrillo_r@hotmail.com>
2021-11-02 16:09:52 -0600

    primer commit

---
+++ helloworld.py
@@ -0,0 +1,48 @@
+# var = 'mundo'
+# num = 1234
+# num2 = 5678
+# flotante = 123.5
+# flotante2 = 456.3
+# caracter = 'c'
+# caracter2 = 'd'
+
+# bol = True
+# bol2 = False
+# bol3 = None
+
+# print(var)
+# print(caracter + caracter2)
+# print(var + caracter)
+
+# print(num + num2)
+# print(flotante + flotante2)
+
+# print(not bol)
+# print(not bol2)
+# print(bol or bol2)
+# print(bol and bol2)
+
+# if 10 > 5:
+#     print('10 es mayor que 5')
+# elif 10 < 5:
+#     print('Eso es imposible')
+
+# if bol:
+#     print('Usamos la variable bol')
+
+# if True:
+#     print("Es verdadero")
+
+# if not False:
+#     print('La negacion de falso')
+
+# var_rapida = 0
+# while bol != bol2:
+#     if var_rapida > 10:
+#         bol = False
+#     print(var_rapida)
+#     var_rapida += 2
+
+for x in range(100): 
+  if not(x%2):
+    print(x)
